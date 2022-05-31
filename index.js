2a14423e040c10f5e82abf7eead537b42fb61566
Katielly <dev.katielly@gmail.com>
2022-05-31 14:21:19 -0400

    atualizações

--- index.js
+++ index.js
@@ -1,51 +1,25 @@
 
-function numeros(numb) {
-    let tela = document.getElementById("tela");
-    // console.log(typeof numb);
-    console.log('tela:', tela)
+function numeros(num) {
+    var tela = document.getElementById('tela').innerHTML;
+    document.getElementById('tela').innerHTML = tela + num;
+}
 
-    switch (numb) {
-        case 0:
-            tela.innerHTML += 0;
-            break;
-        case 1:
-            tela.innerHTML += 1;
-            break;
-        case 2:
-            tela.innerHTML += 2;
-            break;
-        case 3:
-            tela.innerHTML += 3;
-            break;
-        case 4:
-            tela.innerHTML += 4;
-            break;
-        case 5:
-            tela.innerHTML += 5;
-            break;
-        case 6:
-            tela.innerHTML += 6;
-            break;
-        case 7:
-            tela.innerHTML += 7;
-            break;
-        case 8:
-            tela.innerHTML += 8;
-            break;
-        case 9:
-            tela.innerHTML += 9;
-            break;
+function operador(){
 
-    }
-    let valueTela = document.getElementById("tela").value;
-    console.log('final: ', valueTela);
 }
 
 function zerar() {
-    document.getElementById("tela").innerHTML = 0;
+    document.getElementById("tela").innerHTML = "";
 }
 
-function clearNumb (){
-    let display = document.getElementById("tela").value;
-    console.log('display: ', display);
+function back(){
+    let tela = document.getElementById("tela").innerHTML;
+    document.getElementById('tela').innerHTML = tela.substring(0, tela.length -1); //subtring
+}
+
+function result() {
+    let tela = document.getElementById("tela").innerHTML;
+    if(tela){
+        document.getElementById("tela").innerHTML = eval(tela);
+    }
 }
